f68eefcb296892ab22d29b8ec31c852c56b417c1
Rockling Anayo Einstein <160015043+Lordsignet@users.noreply.github.com>
2024-02-14 17:30:10 +0100

    Delete database directory

--- database/base.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbinfo = "mysql:host=localhost;dbname=fhst";
-$dbuser = "fhsttruthwrit";
-$dbpass ="fhst4life@truthwrit";
-$db = new PDO($dbinfo, $dbuser, $dbpass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//prof4NO_$!LackNot$page_in->script = $page_in->addScript("js/editor.js");
-?>
